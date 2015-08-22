73e3b6de363102cfcd28ef29d6008e16e02b8283
k-stz@triton <keistzen@gmail.com>
2015-08-22 14:41:38 +0200

    typos removed

--- 2-chapter/data.lisp
+++ 2-chapter/data.lisp
@@ -6,14 +6,14 @@
 ;; they must fulfill to be a valid representation
 
 ;; a pair, here represented by a closure, a function closing over the input that
-;; will depending on its input retrieve the closed over data. We say the data is
+;; will depending on its input retrieve the closed over data. We say the data
 ;; has a /procedural representation/.
 
 ;; Message passing,
 ;; furthermore the style employed here is called /message passing/ this means we
-;; pass some information to an object or process, here the m-cons returned precedure
+;; pass some information to an object or process, here the m-cons returned procedure
 ;; (a function object), and the object decides what sub-routine to run.
-;; This is to be contrasted by simply calling the routine directly. Basically whneever
+;; This is to be contrasted by simply calling the routine directly. Basically whenever
 ;; a method on an object is invoked we use /message passing/ as the object decides with
 ;; the argument passed "the message" which of its method to invoke.
 
@@ -55,7 +55,7 @@
 ;; (2^a)*(3^b)
 ;; First of how'd we go about retrieving a and b from that term:
 ;; 2^a = (3^b)/(2^a), and finally (log 2^a 2) => a
-;; Which means we need to store 2^a, 3^a and the corresponding logairthm bases
+;; Which means we need to store 2^a, 3^a and the corresponding logarithm bases
 ;; 2 and 3 in the functional object
 
 (defun 2-5-cons (x y)
@@ -80,7 +80,7 @@
 ;; The numbers 2 and 3 are both primes, according to the _fundamental theorem of
 ;; arithmetic_ every positive integer can be represented as the product of primes.
 ;; Also any such product is unique up to the order of factors.
-;; This menas that (* 2^a 3^b) yields a unique numbers. Hence we know that if we
+;; This means that (* 2^a 3^b) yields a unique numbers. Hence we know that if we
 ;; remove either factor (2 or 3) we will yield _integers_ but whenever a factor
 ;; is missing and we remove it we will with absolute certainty get a non integer
 ;; because we will then divide a prime by _a different_ prime, which is always a
@@ -89,7 +89,7 @@
 ;; a number still yields an integer - a modulo test:
 
 (defun remainder-zero-n-times (dividend divisor)
-  "Returns the number of times the divisor can be divided by the divident while at
+  "Returns the number of times the divisor can be divided by the dividend while at
 each division yielding a remainder of zero -- an integer"
   (labels ((rec (quotient n)
 	     (if (= (mod quotient divisor) 0)
@@ -113,10 +113,10 @@ each division yielding a remainder of zero -- an integer"
    3))
 
 
-;; exercise 2.6
+;; exercise 2.6 -- Church numerals ---------------------------------------------
 
-;; this one is weird, the church numerals are representation of numbers using nothing
-;; but procedures. The numeral zero would be:
+;; this one is weird, the church numerals are representation of numbers using nothing but
+;; procedures. The numeral zero would be:
 
 (defun cn-zero (f)
   "Church numeral 0."
