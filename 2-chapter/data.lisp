93c6174ac25cfd618bb28729ee1179b0893189b9
k-stz@triton <keistzen@gmail.com>
2015-08-22 14:38:17 +0200

    more consistent explanation for church numerals

--- 2-chapter/data.lisp
+++ 2-chapter/data.lisp
@@ -183,7 +183,7 @@ each division yielding a remainder of zero -- an integer"
 ;; we should be able to see now that whatever the Church numeral is
 ;; (funcall n f) will always reduce to (λ (f (λ..x)) nesting passed but what
 ;; about the additional (funcall <what-we-discussed> x) around there?
-;; well _that x_ is being passed in the reduced lambda as showed above:
+;; well _that x_ is being passed in the reduced lambda as shown above:
 
 ;; (funcall
 ;;  (lambda (x)
@@ -191,24 +191,29 @@ each division yielding a remainder of zero -- an integer"
 ;;  x) ;;<- _that x_
 ;; ==> (funcall f x) ;; while note (f x) could be an arbitrary nesting indicating a greater
                   ;; church numeral
-;; In effect The both these funcalls effectively expose the x, (f x), (f (f x)) "numeral"
+;; In effect both these funcalls effectively expose the x, (f x), (f (f x)) "numeral"
 ;; part of the procedure!
-;; We could imagine the preceding (f (λ ..)) of every church numeral to be the "gatekeeper"
-;; then the above funcalls pass in the give you admission and papers to deal with the
+;; We could imagine the preceding (f (λ ..)) of every church numeral to be the "gatekeeper part"
+;; then the above funcalls passed give you admission and papers of how to deal with the
 ;; insides of the church numeral representation.
 
 ;; Now that we know how to enter the insides of a church numeral we can try to surgically
 ;; plant another church numeral in there... addition!
+
+;; Given a church numeral: (λf.λx.  f (f (f ... (f x))))
+;; in the following this   ^^^^^^^  ^^^^^^^^^^^^^^^^^
+;; is the "gatekeeper part" and     |||||||||||||||||
+;; is the "numeral part"
 
 (defun cn-add (n m)
   "Add Church numerals n and m"
   (lambda (f)     ;; the "gatekeeper" part of our resulting
     (lambda (x)   ;; church numeral!
       (funcall
-       (funcall m f) ;; <- this will return the m's (λx. (f (f .... x), so we can pass in
-       ;; that x, the non-gatekeeper-part of n, like so:
-       ;; opening the n church numeral's belly, this will return the non-gatekeeper-part
-       ;; of n:
+       (funcall m f) ;; <- this will return m's (λx. (f (f .... x) - numeral part-, so we
+		     ;; can pass in x, into the numeral part of n, like so:
+                     ;;opening the n church numeral's belly, this will return the
+                     ;;numeral-part of n:
        (funcall (funcall n f) x)))))
 
 ;; WORKS: (funcall (funcall (cn-add #'cn-two #'cn-one) #'1+) 2) ==> 5
