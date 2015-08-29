04450a0809c8e5c763ff4eb4b20d85494ced9d17
k-stz@triton <keistzen@gmail.com>
2015-08-29 16:49:08 +0200

    Lem's parallel resistor formula non-identity

--- 2-chapter/interval-arithmetic.lisp
+++ 2-chapter/interval-arithmetic.lisp
@@ -226,3 +226,17 @@
 ;; similarly for lower-bound, which shows that out assumption of adding
 ;; the percentage is approximately the percentage of the product intervals
 
+
+;; Lem E. Tweakit parallel resistor functions
+
+ (defun par1 (r1 r2)
+   (div-interval (mul-interval r1 r2)
+		 (add-interval r1 r2)))
+
+(defun par2 (r1 r2)
+  (let ((one (make-interval 1 1)))
+    (div-interval one
+		  (add-interval (div-interval one r1)
+				(div-interval one r2)))))
+
+;; Lem is right, the above return different results for intervals r1:(1 1) r2:(1 2)
