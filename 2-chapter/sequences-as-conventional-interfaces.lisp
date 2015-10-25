f976bbe43ef735e5c302b66fe9f81a7dfe2125d0
k-stz@triton <keistzen@gmail.com>
2015-10-25 14:54:17 +0100

    exercise 2.34 Horner's rule optiomal algorithm for polynomial evalutation; implementation using ACCUMULATE

--- 2-chapter/sequences-as-conventional-interfaces.lisp
+++ 2-chapter/sequences-as-conventional-interfaces.lisp
@@ -107,3 +107,27 @@
    0 sequence))
 
 
+;; Exercise 2.34
+
+(defun horner-eval (x coefficient-sequence)
+  "Evaluate a polynomial in x at a given value in x. Example: f(x)= 1 + 3x + 5x³ + x^5 at
+x=2 would be (horner-eval 2 (list 1 3 0 5 0 1).  
+
+This function implements Horner's Rule: It can be proven that any algorithm that computes
+arbitrary polynomials must at least use as man additions and multiplications as Horner's
+rule, thus Horner's Rule is an optimal algorithm for polynomial evaluation!
+
+This was proved (for the number additions) by A.M. Otrowski in 1954 paper that essentially
+founded the modern study of optional algorithms. While V.Y.Pan 1966 proved it for
+muliplications."
+  (accumulate (lambda (this-coeff higher-terms) ;;??;;
+		;; it helps to imagnine it "backwards" starting at the initial=0 then, as
+		;; according to Horner's rule "we multiply by x" (* x 0) "then add a.n-1
+		;; (* this-coeff (* x 0)) since we're at the end of the deferred chain
+		;; "this-coeff" is indeed the last value in the coefficient-sequence. Now
+		;; it should be possible to see that the rest of the chain will follow the
+		;; order: (... (a.nx + a.n-1)x + ... +a1)x + a0
+		(+ this-coeff
+		   (* higher-terms x)))
+	      0
+	      coefficient-sequence ))
