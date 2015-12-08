498d052e2e3f7872662ad9d617a7f19a39c5ad81
k-stz@thalassa <keistzen@gmail.com>
2015-12-08 23:19:47 +0100

    package; pic-lang explanation todo

--- 2-chapter/the-picture-language.lisp
+++ 2-chapter/the-picture-language.lisp
@@ -1,5 +1,10 @@
-(defpackage :sicp (:use :cl))
-(in-package :sicp)
+(defpackage :sicp-picture-language
+  (:use :cl
+	:sicp
+	:pic-lang ;; in system: picture-language-package
+	))
+
+(in-package :sicp-picture-language)
 
 ;; Section 2.2.4 Example: the picture language
 
@@ -7,12 +12,19 @@
 ;; We consider a picture language that helps us describe an image that is transformed and
 ;; enclosed by a parallelogram.
 
+;; TODO: rewrite the three properties of language with the acutall example. e.g. means
+;;       of combiniation is a closure who's components are procedures, not lists as until
+;;       now with CONS.
+
 ;; This language has primitives: _the painter_ we call WAVE that draws crude lines
 
-;; Means of combination: e.g. BELOW that takes a WAVEs (painter) as argument and returns a
+;; Means of combination: e.g. BELOW that takes WAVEs (painter) as argument and returns a
 ;; new painter. It has the closure property.
 
 ;; Means of abstraction: BELOW can be abstracted by WAVE2, using a DEFUN
 
 ;; The language uses ordinary Lisp functions and hence allows us to use do everything we
-;; can do with Lisp functions, for example they're in turn first-class objects.
+;; can do with Lisp functions, for example treat them as first-class objects.
+
+
+
