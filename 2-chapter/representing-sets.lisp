8739b4313f4f2e32122561b5fed43f4d7cd7b9b9
k-stz@thalassa <keistzen@gmail.com>
2016-02-01 19:50:20 +0100

    ADJOIN-SET, INTERSECTION-SET added

--- 2-chapter/representing-sets.lisp
+++ 2-chapter/representing-sets.lisp
@@ -6,3 +6,19 @@
 	 (null set) nil) 
 	((equal? x (car set)) t)
 	(t (element-of-set? x (cdr set)))))
+
+
+(defun adjoin-set (x set)
+  (if (element-of-set? x set)
+      set
+      (cons x set)))
+
+(defun intersection-set (set-1 set-2)
+  (cond ((or (null set-1) (null set-2)) '())
+	((element-of-set? (car set-1) set-2)
+	 (cons (car set-1)
+	       (intersection-set (rest set-1)
+				 set-2)))
+	(t
+	 (intersection-set (rest set-1)
+			   set-2))))
