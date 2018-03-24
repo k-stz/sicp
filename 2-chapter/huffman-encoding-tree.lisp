2359bde41249ea403d77a703ab682fe50974929d
k-stz@thalassa <keistzen@gmail.com>
2018-03-24 20:41:35 +0100

    exercise 2.70

--- 2-chapter/huffman-encoding-tree.lisp
+++ 2-chapter/huffman-encoding-tree.lisp
@@ -158,3 +158,22 @@
 (defun generate-huffman-tree (pairs)
   (successive-merge (make-leaf-set pairs)))
 
+;; exercise 2.70
+
+(defparameter  *rock-symbols* '((A 2) (BOOM 1) (GET 2) (JOB 2) (NA 16) (SHA 3) (YIP 9) (WAH 1)))
+
+;; (encode '(get a job
+;; 	  sha na na na na na na na na
+;; 	  get a job
+;; 	  sha na na na na na na na na
+;; 	  Wah yip yip yip yip yip yip yip yip yip
+;; 	  sha boom)
+;; 	(generate-huffman-tree *rock-symbols*))
+==> (1 1 1 1 1 1 1 0 0 1 1 1 1 0 1 1 1 0 0 0 0 0 0 0 0 0 1 1 1 1 1 1 1 0 0 1 1 1 1
+   0 1 1 1 0 0 0 0 0 0 0 0 0 1 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 1 1
+   0 1 1 0 1 1)
+;; Bits needed (length *) ==> 84
+;; What would be the smallest number of bits needed if we used fixed-length code to encode
+;; the eight-symbols of the rock alphabet in *rock-symbols*?
+;; To distinguish 8 different symbols we need (log 8 2) => 3 Bits.
+;; The song has 36 Words so we'd need: (* 3 36) ==> 108 Bits!
