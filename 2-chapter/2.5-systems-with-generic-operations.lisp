dbba267ef0386f98ea6b2514cb169a321b32e861
k-stz@sao <keistzen@gmail.com>
2018-03-29 00:30:53 +0200

    typos

--- 2-chapter/2.5-systems-with-generic-operations.lisp
+++ 2-chapter/2.5-systems-with-generic-operations.lisp
@@ -2,7 +2,7 @@
 
 ;; exercise 2.77
 
-;; tracing thorugh all the calls when issuing (magnitude z)
+;; tracing through all the calls when issuing (magnitude z)
 
 ;; 1. apply-generic 'magnitude is called on z, which will dispatch on
 ;; the the first tag to z, the type 'complex.
@@ -10,9 +10,9 @@
 ;; 2. magnitude is called. But _where_ does it come from?  Its index
 ;; with operation 'magnitude and type 'complex, returning the
 ;; operation, of the same name, 'magnitude.
-;; This magnitude operation isn't the selctor yet, its the complex-arithmetic
+;; This magnitude operation isn't the selector yet, its the complex-arithmetic
 ;; selector (see Ben's and Alyssa's implementation in Section 2.4.3).
-;; Those call an apply-generic interanally:
+;; Those call an apply-generic internally:
 
 ;; 3. apply-generic again, this time dispatching on the next tag, originally the second,
 ;; which is 'rectangular. And this finally returns the actual 'magnitude
