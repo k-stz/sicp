915b8e1822b6887e21ac88df2d923111c1b8a938
k-stz@thalassa <keistzen@gmail.com>
2018-03-30 02:12:26 +0200

    exercise 2.79

--- 2-chapter/2-5-systems-with-generic-operations.lisp
+++ 2-chapter/2-5-systems-with-generic-operations.lisp
@@ -37,6 +37,8 @@
 	    (lambda (x y) (tag (* x y))))
     (put-op 'div '(:cl-number :cl-number)
 	    (lambda (x y) (tag (/ x y))))
+    (put-op 'equ? '(:cl-number :cl-number)
+	    (lambda (x y) (= x y)))
     (put-op 'make :cl-number
 	    (lambda (x) (tag x))))
   'done)
@@ -77,7 +79,9 @@
 	      (lambda (x y) (tag (mul-rat x y))))
       (put-op 'div '(rational rational)
 	      (lambda (x y) (tag (div-rat x y))))
-
+      (put-op 'equ? '(rational rational)
+	      (lambda (x y) (and (= (numer x) (numer y))
+				 (= (denom x) (denom y)))))
       (put-op 'make 'rational
 	      (lambda (n d) (tag (make-rat n d)))))
   'done)
@@ -104,6 +108,9 @@
     (put-op 'imag-part '(rectangular) #'imag-part)
     (put-op 'magnitude '(rectangular) #'magnitude)
     (put-op 'angle '(rectangular) #'angle)
+    (put-op 'equ? '(rectangular)
+	    (lambda (x y) (and (= (real-part x) (real-part y))
+			       (= (imag-part x) (imag-part y)))))
     (put-op 'make-from-real-imag 'rectangular 
 	 (lambda (x y) (tag (make-from-real-imag x y))))
     (put-op 'make-from-mag-ang 'rectangular 
@@ -129,6 +136,9 @@
     (put-op 'imag-part '(polar) #'imag-part)
     (put-op 'magnitude '(polar) #'magnitude)
     (put-op 'angle '(polar) #'angle)
+    (put-op 'equ? '(polar)
+	    (lambda (x y) (and (= (magnitude x) (magnitude y))
+			       (= (angle x) (angle y)))))
     (put-op 'make-from-real-imag 'polar
 	 (lambda (x y) (tag (make-from-real-imag x y))))
     (put-op 'make-from-mag-ang 'polar 
@@ -170,7 +180,11 @@
     (put-op 'mul '(complex complex)
 	 (lambda (z1 z2) (tag (mul-complex z1 z2))))
     (put-op 'div '(complex complex)
-	 (lambda (z1 z2) (tag (div-complex z1 z2))))
+	    (lambda (z1 z2) (tag (div-complex z1 z2))))
+    (put-op 'equ? '(complex complex)
+	    (lambda (x y)
+	      (and (= (apply-generic 'magnitude x) (apply-generic 'magnitude y))
+		   (= (apply-generic 'angle x) (apply-generic 'angle y)))))
     (put-op 'make-from-real-imag 'complex
 	 (lambda (x y) (tag (make-from-real-imag x y))))
     (put-op 'make-from-mag-ang 'complex
@@ -203,3 +217,7 @@
 ;; finally in the cl-number-package (tag x) doesn't do anything to the number, as
 ;; it may call (attach-to :cl-number on-number), but since attach-to simply returns
 ;; the number unaltered when testing for sicp::number? no tag gets attached.
+
+;; exercise 2.79
+;; adding `equ?' and equality operator to the generic-arithmetic package, done by
+;; adding the functions to the packages and exporting them with put-op to the *table-op*
